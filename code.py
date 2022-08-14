bd5794b4cce2800885107c85b745d88d92d64043
Mike MacHenry <mike.machenry@gmail.com>
2022-08-13 21:18:56 -0400

    Implement with state object

--- code.py
+++ code.py
@@ -35,37 +35,50 @@ outputs = [
     ],
 ]
 
-output = None
+colors = [(255,0,0), (0,255,0), (0,0,255)]
 
+# Initialize screen and display battery voltage for one second
 text_area = bitmap_label.Label(terminalio.FONT, scale=2)
 text_area.anchor_point = (0.5, 0.5)
 text_area.anchored_position = (board.DISPLAY.width // 2, board.DISPLAY.height // 2)
-
 main_group = Group()
 main_group.append(text_area)
+board.DISPLAY.show(main_group)
+sensor = LC709203F(board.I2C())
+text_area.text = "Battery:\n{:.1f} Volts \n{}%".format(sensor.cell_voltage, sensor.cell_percent)
+time.sleep(1)
 
 touch = touchio.TouchIn(board.D5)
-
-sensor = LC709203F(board.I2C())
-
-board.DISPLAY.show(main_group)
-
 pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
-#led = digitalio.DigitalInOut(board.LED)
-#led.direction = digitalio.Direction.OUTPUT
+class State:
+    def __init__(self):
+        self.displaying_interaction = False
+        self.mode = 0
+        self.num_modes = len(outputs)
+        self._show_welcome_screen()
+        self._indicate_mode()
 
-class Mode:
-    def __init__ (self, num):
-        self.value = 0
-        self.num = num
+    def mode_button(self):
+        if self.displaying_interaction:
+            self.displaying_interaction = False
+            self._show_welcome_screen()
+        else:
+            self.mode = (self.mode + 1) % self.num_modes
+            self._indicate_mode()
+            
+    def touch_button(self):
+        if not self.displaying_interaction:
+            self.displaying_interaction = True
+            text_area.text = random.choice(outputs[self.mode])
+    
+    def _show_welcome_screen(self):
+        text_area.text = "Press for an\ninteraction"
+        
+    def _indicate_mode(self):
+        pixel.fill(colors[self.mode])
 
-    def next (self):
-        self.value = (self.value + 1) % self.num
-
-mode = Mode(3)
-
-colors = [(255,0,0), (0,255,0), (0,0,255)]
+state = State()
 
 async def button_rising_edge ():
     global output
@@ -74,44 +87,19 @@ async def button_rising_edge ():
             event = keys.events.get()
             if event:
                 if event.pressed:
-                    if output:
-                        output = None
-                    else:
-                        mode.next()
-                        pixel.fill(colors[mode.value])
-                #elif event.released:
-                #    pass
+                    state.mode_button()
             await asyncio.sleep(0)
 
-async def capacitive_touch_rising_edge():
+async def touch_rising_edge():
     global output
     while True:
         if touch.value:
-            if not output:
-                output = random.choice(outputs[mode.value])
+            state.touch_button()
         await asyncio.sleep(0)
 
-def display_battery ():
-    text_area.text = "Battery:\n{:.1f} Volts \n{}%".format(sensor.cell_voltage, sensor.cell_percent)
-    time.sleep(1)
-
-async def display_output ():
-    while True:
-        if output:
-            text = output
-        else:
-            text = "Press for an\ninteraction"
-        
-        if text_area.text != text:
-            text_area.text = text
-        await asyncio.sleep(1)
-
 async def main ():
-    display_task = asyncio.create_task(display_output())
     button_task = asyncio.create_task(button_rising_edge())
-    touch_task = asyncio.create_task(capacitive_touch_rising_edge())
-    await asyncio.gather(display_task, button_task, touch_task)
+    touch_task = asyncio.create_task(touch_rising_edge())
+    await asyncio.gather(button_task, touch_task)
 
-display_battery()
-pixel.fill(colors[mode.value])
 asyncio.run(main())
